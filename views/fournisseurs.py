dd99bdb2222a1dd497edc48f96adb331a1b272f4
HalfAlmighty <guillaume.saucy@gmail.com>
2025-11-11 00:21:42 +0100

    Update fournisseurs.py

--- views/fournisseurs.py
+++ views/fournisseurs.py
@@ -16,18 +16,18 @@ def show():
     ]
     df = pd.DataFrame(data)
 
-    familles = df['famille'].unique()
-    famille_selected = st.sidebar.selectbox("Choisir une famille", familles)
+    fournisseur = df['fournisseur'].unique()
+    fournisseur_selected = st.sidebar.selectbox("Choisir un fournisseur", fournisseur)
 
-    fournisseurs_list = df[df['famille'] == famille_selected]['fournisseur'].unique()
-    for fournisseur in fournisseurs_list:
-        lots = df[(df['famille'] == famille_selected) & (df['fournisseur'] == fournisseur)]['lot'].tolist()
+    familles_list = df[df['fournisseur'] == fournisseur_selected]['famille'].unique()
+    for famille in familles_list:
+        lots = df[(df['fournisseur'] == fournisseur_selected) & (df['famille'] == famille)]['lot'].tolist()
         with st.expander(fournisseur):
-            if fournisseur == "LINDE FRANCE":
-                linde_france.show(famille_selected, lots)
-            elif fournisseur == "ROTH SOCHIEL":
-                roth_sochiel.show(famille_selected, lots)
-            elif fournisseur == "UGAP":
-                ugap.show(famille_selected, lots)
-            elif fournisseur == "PARA MEDICAL HYGIENE":
-                para_medical_hygiene.show(famille_selected, lots)
+            if famille == "BOUTEILLE DE GAZ QUALITE INDUSTRIEL":
+                linde_france.show(fournisseur_selected, lots)
+            elif famille == "CONSOMMABLE POUR LA BIOLOGIE" && lots == "Lot A":
+                roth_sochiel.show(fournisseur_selected, lots)
+            elif famille == "CONSOMMABLE POUR LA BIOLOGIE" && lots == "Lot B":
+                ugap.show(fournisseur_selected, lots)
+            elif famille == "CONSOMMABLE POUR L'HYGIENE ET LA SECURITE - EPI" && lots == "Lot C":
+                para_medical_hygiene.show(fournisseur_selected, lots)
